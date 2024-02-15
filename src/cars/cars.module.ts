1bd84410d35aa1beb4d7ea01f735af42daf5b0d6
Solwen02 <121789865+Solwen02@users.noreply.github.com>
2024-02-15 09:54:15 +0100

    add schema and dto

--- src/cars/cars.module.ts
+++ src/cars/cars.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
-import { CarsModule } from './cars/cars.module'
+import { CarsController } from './cars.controller'
+import { CarsService } from './cars.service'
 
 @Module({
     imports: [
         MongooseModule.forRoot('mongodb://localhost:27017/test'),
         CarsModule,
     ],
+    controllers: [CarsController],
+    providers: [CarsService],
 })
-export class AppModule {}
+export class CarsModule {}
---
+++ src/cars/schema/car.schema.ts
@@ -0,0 +1,18 @@
+import { HydratedDocument } from 'mongoose'
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+
+export type CarDocument = HydratedDocument<Car>
+
+@Schema()
+export class Car {
+    @Prop()
+    brand: string
+
+    @Prop()
+    price: string
+
+    @Prop()
+    description: string
+}
+
+export const CarSchema = SchemaFactory.createForClass(Car)
