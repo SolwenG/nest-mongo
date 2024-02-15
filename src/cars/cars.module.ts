6625b222889165d87adade717e4131bd9f13d542
Solwen02 <121789865+Solwen02@users.noreply.github.com>
2024-02-15 10:30:33 +0100

    corrected module

--- src/cars/cars.module.ts
+++ src/cars/cars.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { CarsController } from './cars.controller'
 import { CarsService } from './cars.service'
+import { Car, CarSchema } from './schema/car.schema'
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb://localhost:27017/test')],
+    imports: [
+        MongooseModule.forFeature([{ name: Car.name, schema: CarSchema }]),
+    ],
     controllers: [CarsController],
     providers: [CarsService],
 })
